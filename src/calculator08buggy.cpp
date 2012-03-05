6cc5bc2953be5ac58a5539dcfc9270ace1d70757
Travis Carr <tmcarr@tmcarr.com>
2012-03-05 14:03:35 -0600

    reformatted

--- src/calculator08buggy.cpp
+++ src/calculator08buggy.cpp
@@ -44,26 +44,10 @@ Token Token_stream::get()
 	char ch;
 	cin >> ch;
 	switch (ch) {
-	case '(':
-	case ')':
-	case '+':
-	case '-':
-	case '*':
-	case '/':
-	case '%':
-	case ';':
+	case '(':case ')':case '+':case '-':case '*':case '/':case '%':case ';':
 	case '=':
 		return Token(ch);
-	case '.':
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
+	case '.':case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':
 	case '9':
 	{	cin.unget();
 		double val;
