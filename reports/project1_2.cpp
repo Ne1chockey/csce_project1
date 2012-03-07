974468e6b881fed2267e304701ac8763e3b6f33f
Travis Carr <tmcarr@tmcarr.com>
2012-03-06 20:01:04 -0600

    Saved the copy for submission in reports

---
+++ reports/project1_2.cpp
@@ -0,0 +1,294 @@
+
+/*
+	calculator08buggy.cpp
+
+	Helpful comments removed.
+
+	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
+*/
+
+#include "std_lib_facilities.h"
+using namespace std;
+
+//define token struct
+struct Token {
+	char kind;
+	double value;
+	string name;
+	Token(char ch) :kind(ch), value(0) { }
+	Token(char ch, double val) :kind(ch), value(val) { }
+	// Not sure if this will work, but worth a try... 
+	// Seems to work
+	Token(char ch, string val) :kind(ch), name(val) { }
+};
+
+//define token_stream class
+class Token_stream {
+	bool full;
+	Token buffer;
+public:
+	Token_stream() :full(0), buffer(0) { 
+        }
+	Token get();
+	void unget(Token t) { 
+                buffer=t; full=true; 
+        }
+	void ignore(char);
+};
+
+//define constants used for controlling the program
+const char let = 'L';
+const char quit = 'Q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+const char root = 'S';
+const char help = 'H';
+const char pows = 'P';
+
+//actually define get() function in Token class
+Token Token_stream::get()
+{
+	if (full) { full=false; return buffer; }
+	char ch;
+	cin >> ch;
+	switch (ch) {
+		case '(':case ')':case '+':case '-':case '*':case '/':case '%':case ';':case ',':
+		case '=':
+			return Token(ch);
+		case '.':case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':
+		case '9': {	
+			cin.unget();
+			double val;
+			cin >> val;
+			return Token(number,val);
+		}
+		case 'k': {
+			cin.unget();
+			char val;
+			cin >> val;
+			return Token(number,1000);
+		}
+		default: {
+			if (isalpha(ch)) {
+				string s;
+				s += ch;
+				//Error on 78 but not sure whats up. Why are we checking for both digits and chars? WTH? Is the + operator already overloaded in std_lib_facilities?
+				while(cin.get(ch) && (isalpha(ch) || isdigit(ch))) 
+                                        s+=ch;
+				cin.unget();
+				if (s == "let") return Token(let);
+				// name is not quit.... 	
+				if (s == "quit") return Token(quit);
+				if (s == "sqrt") return Token(root);
+				if (s == "help") return Token(help);
+				if (s == "pow")  return Token(pows);
+				return Token(name,s);
+			}
+			error("Bad token");
+		}
+	}
+}
+
+void Token_stream::ignore(char c){
+	if (full && c==buffer.kind) {
+		full = false;
+		return;
+	}
+	full = false;
+
+	char ch;
+	while (cin>>ch)
+		if (ch==c) return;
+}
+
+struct Variable {
+	string name;
+	double value;
+	Variable(string n, double v) :name(n), value(v) { }
+};
+
+vector<Variable> names;	
+
+double get_value(string s) {
+	for (int i = 0; i<names.size(); ++i)
+		if (names[i].name == s) return names[i].value;
+	error("get: undefined name ",s);
+}
+
+void set_value(string s, double d){
+	for (int i = 0; i<names.size(); ++i)
+		if (names[i].name == s) {
+			names[i].value = d;
+			return;
+		}
+	error("set: undefined name ",s);
+}
+
+bool is_declared(string s){
+	for (int i = 0; i<names.size(); ++i)
+		if (names[i].name == s) return true;
+	return false;
+}
+
+Token_stream ts;
+
+double expression();
+
+double primary(){
+	Token t = ts.get();
+	switch (t.kind) {
+		case '(': {
+			double d = expression();
+			t = ts.get();
+			if (t.kind != ')') error("')' expected");
+			return d;
+		}
+		case '-':
+			return - primary();
+
+		// for sqrt
+		case root: {
+			double d = expression();
+			if(d<0)error("cant be negative");
+			else{
+				double final = sqrt(d);
+				return final;
+			}
+		}
+		case pows: {
+			t=ts.get();
+	        if (t.kind != '(') error("'(' expected");
+	        double d=expression();
+	        t=ts.get();
+	        if (t.kind!=',') error("',' expected");
+	        double i=expression();
+	        t=ts.get();
+	        if (t.kind != ')') error("')' expected");
+	        return pow(d,i);
+		}
+		case help: {
+			cout << "\nYou are allowed to use +,-,*,/,sqrt(), and pow()\n\n";
+			cout << "Once you are done entering an expression, enter a ';' to execute the expression.\n";
+			cout << "pow() is used in the following syntax: pow(<number to be raised>,<number to raise it to>)\n\n";
+		}
+	    // number = '8'
+		case number:
+			return t.value;
+		// name = 'a'
+		case name:
+			return get_value(t.name);
+		case '=':
+			return primary();
+		default:
+			error("primary expected");
+	}
+}
+
+double term(){
+	double left = primary();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '*':
+			left *= primary();
+			break;
+		case '/':
+		{	double d = primary();
+			if (d == 0) error("divide by zero");
+			left /= d;
+			break;
+		}
+		default:
+			ts.unget(t);
+			return left;
+		}
+	}
+}
+
+double expression(){
+	double left = term();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '+':
+			left += term();
+			break;
+		case '-':
+			left -= term();
+			break;
+		default:
+			ts.unget(t);
+			return left;
+		}
+	}
+}
+
+double declaration(){
+	Token t = ts.get();
+	if (t.kind != 'a') error ("name expected in declaration");
+	string name = t.name;
+	if (is_declared(name)) error(name, " declared twice");
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of " ,name);
+	double d = expression();
+	names.push_back(Variable(name,d));
+	return d;
+}
+
+double statement(){
+	Token t = ts.get();
+	switch(t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.unget(t);
+		return expression();
+	}
+}
+
+void clean_up_mess(){
+	ts.ignore(print);
+}
+
+const string prompt = "> ";
+const string result = "= ";
+
+void calculate(){
+	while(true) try {
+		cout << prompt;
+		Token t = ts.get();
+
+
+
+		// print = ';'
+		while (t.kind == print) t=ts.get();
+		// quit = 'Q'
+		if (t.kind == quit) return;
+		ts.unget(t);
+		cout << result << statement() << endl;
+	}
+	catch(runtime_error& e) {
+		cerr << e.what() << endl;
+		clean_up_mess();
+	}
+}
+
+int main(){
+	try {
+		calculate();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "exception: " << e.what() << endl;
+		char c;
+		while (cin >>c&& c!=';') ;
+		return 1;
+	}
+	catch (...) {
+		cerr << "exception\n";
+		char c;
+		while (cin>>c && c!=';');
+		return 2;
+	}
+}
